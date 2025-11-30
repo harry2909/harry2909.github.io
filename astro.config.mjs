e06649008b5aa34f4f407bb9e972dd6f2a29c4dd
Harry Preston <hpreston2909@gmail.com>
2025-11-30 17:01:12 +0000

    Update actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  // Update this with your GitHub username and repository name
-  // If your repo is "username.github.io", use base: "/"
-  // Otherwise, use base: "/repo-name/"
-  site: 'https://YOUR_USERNAME.github.io',
-  base: '/ruby-portfolio/', // Change this to "/" if repo is username.github.io
+  site: 'https://harry2909.github.io',
+  base: '/ruby-portfolio',
   vite: {
     plugins: [tailwindcss()]
   }
